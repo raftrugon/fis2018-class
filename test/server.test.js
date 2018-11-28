8f154c567a95b9368895b952450cd3cb20f65257
Rafa <rafatgonzalez@gmail.com>
2018-11-28 19:36:59 +0100

    Archivos de test

---
+++ test/server.test.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Contacts API', () => {
+    it('hola mundo de prueba', (done) => {
+        var x = 3;
+        var y = 5;
+
+        var resultado = x + y;
+
+        expect(resultado).eql(8);
+        done();
+    });
+});
